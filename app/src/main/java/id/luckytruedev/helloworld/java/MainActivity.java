090163c4b30c566abe8768d73982862d3889fcc0
poncoe <poncoe4mac@gmail.com>
2019-01-20 00:49:36 +0700

    Added Kotlin

--- app/src/main/java/id/luckytruedev/helloworld/java/MainActivity.java
+++ app/src/main/java/id/luckytruedev/helloworld/java/MainActivity.java
@@ -1,7 +1,9 @@
-package id.luckytruedev.helloworld;
+package id.luckytruedev.helloworld.java;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import id.luckytruedev.helloworld.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/id/luckytruedev/helloworld/kotlin/MainActivityKotlin.kt
@@ -0,0 +1,13 @@
+package id.luckytruedev.helloworld.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import id.luckytruedev.helloworld.R
+
+class MainActivityKotlin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
